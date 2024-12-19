3019dcf9ad37781463afd9859b73004986ced4e8
med4h <adrian.meda6@gmail.com>
2024-12-19 08:19:25 -0800

    added invoice inputes

---
+++ main.py
@@ -0,0 +1,41 @@
+from customtkinter import *
+
+app = CTk()
+app.geometry("800x400")
+app.title("Multiverse Invoice Generator Project")
+
+frame = CTkFrame(app)
+frame.pack()
+
+first_name_label = CTkLabel(frame, text="First Name")
+first_name_label.grid(row=0, column=0)
+last_name_label = CTkLabel(frame, text="Last Name")
+last_name_label.grid(row=0, column=1)
+
+first_name_entry = CTkEntry(frame)
+last_name_entry = CTkEntry(frame)
+first_name_entry.grid(row=1, column=0)
+last_name_entry.grid(row=1, column=1)
+
+
+phone_label = CTkLabel(frame, text="Phone")
+phone_label.grid(row=0, column=2)
+phone_entry = CTkEntry(frame)
+phone_entry.grid(row=1, column=2)
+
+qty_label = CTkLabel(frame, text="Qty")
+qty_label.grid(row=2, column=0)
+qty_entry = CTkEntry(frame)
+qty_entry.grid(row=3, column=0)
+
+description_label = CTkLabel(frame, text="Description")
+description_label.grid(row=2, column=1)
+description_entry = CTkEntry(frame)
+description_entry.grid(row=3, column=1)
+
+unit_price_label = CTkLabel(frame, text="Unit Price")
+unit_price_label.grid(row=2, column=2)
+unit_price_entry = CTkEntry(frame)
+unit_price_entry.grid(row=3, column=2)
+
+app.mainloop()
